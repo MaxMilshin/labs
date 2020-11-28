830c509d9bb3be7bd112b068a0dfd6cf8dce7561
milshin.maksim <milshin.maksim@88935047-e299-4ba0-ae8b-38ca72f908ea>
2020-11-28 20:27:37 +0000

    version 2.0

--- milshin.maksim/lab_03/src/mergesort.c
+++ milshin.maksim/lab_03/src/mergesort.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void assigment(char* arr1, char* arr2, size_t elem_sz) {
-	for (int i = 0; i < elem_sz; i++) 
-		arr1[i] = arr2[i];
-}
+#include <assert.h>
+#include <string.h>
 
 void mergesort(void* arr, int count, size_t elem_sz, int (*cmp)(const void *, const void *)) {
 	if (count <= 1)
@@ -15,28 +12,27 @@ void mergesort(void* arr, int count, size_t elem_sz, int (*cmp)(const void *, co
 	mergesort(arr, left_count, elem_sz, cmp);
     mergesort(right_arr, count - left_count, elem_sz, cmp);
 	char* brr = (char *) malloc(count * elem_sz);
+	assert (brr != NULL);
 	int i = 0, j = 0;
 	while (i < left_count && j < right_count) {
 		if (cmp(&char_arr[i * elem_sz], &right_arr[j * elem_sz]) < 0) {
-			assigment(&brr[(i + j) * elem_sz], &char_arr[i * elem_sz], elem_sz);
+			memcpy(&brr[(i + j) * elem_sz], &char_arr[i * elem_sz], elem_sz);
 			i++;
 		} 
 		else {
-			assigment(&brr[(i + j) * elem_sz], &right_arr[j * elem_sz], elem_sz);
+			memcpy(&brr[(i + j) * elem_sz], &right_arr[j * elem_sz], elem_sz);
 			j++;
 		}
 	}
 	while (i < left_count) {
-		assigment(&brr[(i + j) * elem_sz], &char_arr[i * elem_sz], elem_sz);
+		memcpy(&brr[(i + j) * elem_sz], &char_arr[i * elem_sz], elem_sz);
 		i++;
 	}
 	while (j < right_count) {
-		assigment(&brr[(i + j) * elem_sz], &right_arr[j * elem_sz], elem_sz);
+		memcpy(&brr[(i + j) * elem_sz], &right_arr[j * elem_sz], elem_sz);
 		j++;
 	}
-	for (int i = 0; i < count; i++) {
-		assigment(&char_arr[i * elem_sz], &brr[i * elem_sz], elem_sz);
-	}
+	memcpy(char_arr, brr, count * elem_sz);
 	free(brr);
 	return;
 }
