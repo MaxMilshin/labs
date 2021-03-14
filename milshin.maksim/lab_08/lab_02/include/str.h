26d24cc259325485945d6f1fc910926f2abe58c9
milshin.maksim <milshin.maksim@88935047-e299-4ba0-ae8b-38ca72f908ea>
2021-03-14 08:42:35 +0000

    delete rubbish

--- milshin.maksim/lab_08/lab_02/include/str.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-char* my_strcpy(char* dest, const char* src);
-
-char* my_strcat(char* dest, const char* src);
-
-int my_strcmp(const char* s1, const char* s2);
-
-int my_strlen(const char* s);
