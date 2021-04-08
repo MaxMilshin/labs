374bb08bcf5ff6f0f3bc3117b7260ac81f0935ff
milshin.maksim <milshin.maksim@88935047-e299-4ba0-ae8b-38ca72f908ea>
2021-04-08 13:37:27 +0000

    v1.0

---
+++ milshin.maksim/lab_12/include/matrix.hpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Matrix {
+public:
+	Matrix(std::size_t rows, std::size_t cols);
+	~Matrix() noexcept = default;
+
+	void operator = (Matrix other);
+
+	static Matrix* load(std::ifstream &);
+	
+	void print() const noexcept;
+
+	void add(const Matrix & other);
+	void multiply(const Matrix & other);
+
+	int get_element(int row, int col) const;
+
+private:
+	friend std::ifstream& operator >>(std::ifstream &, Matrix*);
+	void operator +=(const Matrix & other) noexcept;
+	Matrix* operator *(const Matrix & other) const noexcept;
+
+private:
+	std::size_t rows, cols;
+	std::vector<std::vector<int>> data;
+};
+
+
+class MatrixException {
+public:
+	MatrixException(std::string message) : message(message) {}
+	~MatrixException() = default;
+	
+	std::string get_message() const {
+		return message;
+	}
+
+private:
+	std::string message;
+};
---
+++ milshin.maksim/lab_12/src/matrix.cpp
@@ -0,0 +1,91 @@
+#include <fstream>
+#include <vector>
+
+#include "matrix.hpp"
+
+Matrix::Matrix(std::size_t rows, std::size_t cols) : 
+	rows(rows), cols(cols), 
+	data(std::vector<std::vector<int>>(rows, std::vector<int>(cols))) {}
+
+void Matrix::operator = (Matrix other) {
+	std::swap(data, other.data);
+	std::swap(rows, other.rows);
+	std::swap(cols, other.cols);
+}
+
+
+Matrix* Matrix::load(std::ifstream &in) {
+	std::size_t rows, cols;
+	if (!(in >> rows >> cols)) {
+		// std::cout << rows << " " << cols << std::endl;
+		throw MatrixException("LOAD: invalid file format.");
+	} 
+	// std::cout << rows << " " << cols << std::endl;
+	Matrix* matrix = new Matrix(rows, cols);
+	in >> matrix;
+	return matrix;
+}
+
+std::ifstream& operator >>(std::ifstream &in, Matrix* matrix) {
+	for (std::size_t i = 0; i < matrix->rows; i++) {
+		for (std::size_t j = 0; j < matrix->cols; j++) {
+			if (!(in >> matrix->data[i][j])) {
+				throw MatrixException("LOAD: invalid file format.");
+			}
+		}
+	}
+	return in;
+}
+
+
+void Matrix::print() const noexcept {
+	for (std::size_t i = 0; i < rows; i++) {
+		for (std::size_t j = 0; j < cols; j++) {
+			std::cout << data[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void Matrix::add(const Matrix & other) {
+	if (rows != other.rows || cols != other.cols) {
+		throw MatrixException("ADD: dimensions do not match."); 
+	}
+	std::cout << "Im here" << std::endl;
+	*this += other; 
+}
+
+void Matrix::operator +=(const Matrix & other) noexcept {
+	for (std::size_t i = 0; i < rows; i++) {
+		for (std::size_t j = 0; j < cols; j++) {
+			data[i][j] += other.data[i][j];
+			std::cout << "now Im here" << std::endl;
+		}
+	}
+}
+
+void Matrix::multiply(const Matrix & other) {
+	if (cols != other.rows) {
+		throw MatrixException("MUL: #arg1.columns != #arg2.rows.");
+	}
+	*this = *(*this * other);
+}
+
+Matrix* Matrix::operator *(const Matrix & other) const noexcept {
+	Matrix* product = new Matrix(rows, other.cols);
+	for (std::size_t i = 0; i < rows; i++) {
+		for (std::size_t j = 0; j < other.cols; j++) {
+			for (std::size_t k = 0; k < cols; k++) {
+				product->data[i][j] += data[i][k] * other.data[k][j]; 
+			}
+		}
+	}
+	return product;
+}
+
+int Matrix::get_element(int row, int col) const {
+	if (row >= rows || row < 0 || col >= cols || col < 0) {
+		throw MatrixException("ACCESS: bad index.");
+	}
+	return data[row][col];
+}
